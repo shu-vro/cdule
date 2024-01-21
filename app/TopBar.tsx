886e761c3b91cfe61cc8d2ab17af4e3cf520b3fa
shirshen <official.shirshen@gmail.com>
2024-01-22 01:43:15 +0600

    feat: add install button for pwa

--- app/TopBar.tsx
+++ app/TopBar.tsx
@@ -2,20 +2,43 @@
 
 import { useNavbar } from "@/contexts/NavbarContext";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa6";
 import favicon from "./favicon.ico";
+import { GoDesktopDownload } from "react-icons/go";
 
 export default function TopBar() {
     const { setValue } = useNavbar();
+    const [deferredPrompt, setDeferredPrompt] =
+        useState<BeforeInstallPromptEvent>();
+
+    useEffect(() => {
+        window.addEventListener("beforeinstallprompt", e => {
+            setDeferredPrompt(e as BeforeInstallPromptEvent);
+        });
+    }, []);
     return (
-        <div className="sticky top-0 bg-[#333] w-full h-16 flex justify-between items-center text-3xl px-4">
-            <FaBars
-                className="cursor-pointer"
-                onClick={() => {
-                    setValue(true);
-                }}
-            />
+        <div className="sticky top-0 bg-[#333] w-full h-16 flex justify-center items-center text-3xl px-4">
+            <div className="grow flex flex-row">
+                <FaBars
+                    className="cursor-pointer"
+                    onClick={() => {
+                        setValue(true);
+                    }}
+                />
+                {deferredPrompt && (
+                    <GoDesktopDownload
+                        className="cursor-pointer ml-4"
+                        onClick={async () => {
+                            deferredPrompt.prompt();
+                            const { outcome } = await deferredPrompt.userChoice;
+                            if (outcome === "accepted") {
+                                setDeferredPrompt(undefined);
+                            }
+                        }}
+                    />
+                )}
+            </div>
             <h1 className="font-bold flex flex-row justify-center items-center">
                 C<span className="text-[#b16ced]">DULE</span>
                 <Image
